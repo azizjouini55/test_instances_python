1c58bea197c2998d2d26651d7414881ea0f27d3d
aziz <aziz@gmail.com>
2023-01-23 22:53:13 +0100

    working average and max trip

--- input_read.py
+++ input_read.py
@@ -18,12 +18,12 @@ def read_input_pdptw(filename):
         elif token == "MAX_TRIP_PER_BUS":
             next(file_it)  # Removes the ":"
             max_trips_per_bus = int(next(file_it))
-        elif token == "MAX_SCHOOLS_PER_BUS":
-            next(file_it)  # Removes the ":"
-            max_schools_per_bus = int(next(file_it))
         elif token == "NUMBER_OF_BUSES":
             next(file_it)  # Removes the ":"
-            nb_trucks = int(next(file_it))
+            nb_buses = int(next(file_it))
+        elif token == "MIN_BUS_UTILIZATION":
+            next(file_it)  # Removes the ":"
+            min_bus_utilization = int(next(file_it))
         elif token == "SPEED":
             next(file_it)  # Removes the ":"
             speed = int(next(file_it))
@@ -34,10 +34,10 @@ def read_input_pdptw(filename):
             break
     
     # adding bus capacitites
-    truck_capacitites = []
-    for truck_index in range(nb_trucks):
+    buses_capacitites = []
+    for bus_index in range(nb_buses):
         next(file_it)
-        truck_capacitites.append(int(next(file_it)))
+        buses_capacitites.append(int(next(file_it)))
     
     next(file_it)
     
@@ -54,8 +54,9 @@ def read_input_pdptw(filename):
     for school_index in range(nb_schools):
         school_index = int(next(file_it))
         same_school_indexes = []
+      
         for index in range(school_indexes[school_index - 1]):
-            same_school_indexes.append(int(next(file_it)))
+            same_school_indexes.append(int(float(next(file_it))))
         school_nodes.append(same_school_indexes)
         
     next(file_it)
@@ -69,37 +70,44 @@ def read_input_pdptw(filename):
 
     max_horizon = int(next(file_it))
 
-    customers_x = []
-    customers_y = []
+    students_x = []
+    students_y = []
     demands = []
     earliest_start = []
     latest_end = []
     service_time_matrix = [service_time] * nb_students
     pick_up_index = []
     delivery_index = []
+    grade_index = []
 
     while True:
         val = next(file_it, None)
         if val is None:
             break
         i = int(val) - 1
-        customers_x.append(float(next(file_it)))
-        customers_y.append(float(next(file_it)))
+        students_x.append(float(next(file_it)))
+        students_y.append(float(next(file_it)))
         demands.append(int(next(file_it)))
         ready = int(next(file_it))
         due = int(next(file_it))
         pick = int(next(file_it))
         delivery = int(next(file_it))
+        grade = next(file_it)
         earliest_start.append(ready)
         # in input files due date is meant as latest start time
         latest_end.append(due + service_time)
         pick_up_index.append(pick - 1)
         delivery_index.append(delivery - 1)
+        grade_index.append(grade)
+
     
-    
-    return nb_students, nb_trucks, truck_capacitites, \
-            demands, service_time_matrix, earliest_start, latest_end, pick_up_index, \
-            delivery_index, max_horizon,school_nodes, max_trips_per_bus, max_schools_per_bus
+    #with open("distance_matrix", "wb") as fp:   #Pickling
+ 
+    return nb_students, nb_buses, buses_capacitites,  \
+            demands,  earliest_start, latest_end, pick_up_index, \
+            delivery_index, max_horizon,   school_nodes, max_trips_per_bus, min_bus_utilization
+
+
 
 
 def read_file_output(filename):
@@ -112,29 +120,35 @@ def read_file_output(filename):
         sequences.append( list(map(int, str_sequence)))
     return sequences
 
-def main(solution_file):
+def main(instance_file,solution_file):
     
     #Read instance data
-    # nb_students, nb_trucks, truck_capacity, \
-    # demands_data, service_time_data, earliest_start_data, latest_end_data, \
-    # pick_up_index, delivery_index, max_horizo ,\
-    # school_nodes, max_trips_per_bus, max_schools_per_bus = read_input_pdptw(instance_file)
+    nb_students, nb_buses, buses_capacitites, \
+        demands,  earliest_start, latest_end, pick_up_index, \
+        delivery_index, max_horizon,  school_nodes, max_trips_per_bus, min_bus_utilization = read_input_pdptw(instance_file)
     solution_sequences=read_file_output(solution_file)
+    school_nodes_list = []
+    for sublist in school_nodes:
+        for item in sublist:
+            school_nodes_list.append(item)
     # print("nb of students :"+str(nb_students))
     # print("max trips :"+str( max_trips_per_bus))
     # print("school nodes :"+str( school_nodes))
     # print("pick up index :"+str( pick_up_index))
     # print("delivery index :"+str( delivery_index))
-    # print("truck capcity :"+str( truck_capacity))
-    # print("nb trucks :"+str( nb_trucks))
-    print(solution_sequences)
+    # print("truck capcity :"+str( buses_capacitites))
+    # print("nb trucks :"+str( nb_buses))
+    # print(solution_sequences)
+   
+
+    return solution_sequences,buses_capacitites,school_nodes_list,max_trips_per_bus
     
    
 if __name__ == "__main__" :
 
-    instance_file = "lc101_instance1.txt"
-    soltion_file="Lc101_solution.txt"
-main(soltion_file)
+    instance_file="lc101_instance1.txt"
+    soltion_file="lc101_instance_S.txt"
+    main(instance_file,soltion_file)
 
   
    
--- test_instance.py
+++ test_instance.py
@@ -2,15 +2,18 @@
 import pytest
 
 
-#import input_read
+import input_read
 
 
-def test_max_trip_done(sequence,school_node,max_trip):
+def test_max_trip(sequence,school_node,max_trip):
     trip_counter=0
     for i in range(1,len(sequence)):
         if sequence[i] in school_node and sequence[i-1] not in school_node:
             trip_counter+=1
-    return trip_counter==max_trip
+    if trip_counter!=max_trip:
+        return "max trips surpassed, reached {}".format(trip_counter)
+    return "max trips not surpassed, reached {}".format(trip_counter)
+
 
 def test_school_visit(sequence,school_nodes):
     test_nodes=list()
@@ -23,7 +26,7 @@ def test_all_students_same_school():
     pass
 
 
-def test_bus_capacity(sequence,capacity,school_nodes):
+def test_bus_capacity(bus_number,sequence,capacity,school_nodes):
     min_capacity=0.7
     total_capacity=0
     drop_off=0
@@ -33,15 +36,16 @@ def test_bus_capacity(sequence,capacity,school_nodes):
             drop_off+=1
             if drop_off==pick_up!=0  :
                 if total_capacity<=min_capacity:
-                    return '{} percent capacity not reached on bus '.format(min_capacity*100)
+                    return '{} percent capacity not reached on bus {}'.format(min_capacity*100,bus_number)
                 total_capacity=0
                 pick_up=0
                 drop_off=0
         elif sequence[i] not in school_nodes:
             pick_up+=1
             total_capacity+=1/capacity 
+    return 'reached 70 percent capacity during all trips'
   
-def test_bus_capacity_average(sequence,capacity,school_nodes):
+def test_bus_capacity_average(bus_number,sequence,capacity,school_nodes):
     total_capacity=0
     drop_off=0
     pick_up=0
@@ -57,11 +61,24 @@ def test_bus_capacity_average(sequence,capacity,school_nodes):
                 trip_counter+=1
                 pick_up=0
                 drop_off=0
-        elif sequence[i] not in school_nodes:
+        else:
             pick_up+=1
             total_capacity+=1
-    return average/trip_counter
+    return "total trips {} , average cpacity of {} percent".format(trip_counter,bus_number,average/trip_counter*100)
+
+def iterate_solution_sequences(instance_file,solution_file):
+    file=list()
+    solution_sequences,buses_capacitites,school_nodes,max_trip=input_read.main(instance_file,solution_file)
+    for sequence in range(1,len(solution_sequences)):
+        print(test_bus_capacity_average(sequence,solution_sequences[sequence],buses_capacitites[sequence-1],school_nodes))
+        print(test_bus_capacity(sequence,solution_sequences[sequence],buses_capacitites[sequence-1],school_nodes)) 
+        print(test_max_trip(solution_sequences[sequence],school_nodes,max_trip))
+        print("////////////////")
+    
 
 
-print(test_bus_capacity([3, 8, 1, 2, 6, 7],2,[ 8,  6, 7]))
-print(test_bus_capacity_average([3, 8, 1, 2, 6, 7],2,[ 8,  6, 7]))
+
+
+instance_file="lc101_instance1.txt"
+soltion_file="lc101_instance_S.txt"
+iterate_solution_sequences(instance_file,soltion_file)
